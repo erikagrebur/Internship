93a344386b6bf14872e29aecf17c806ee0d3bb43
Erika Grebur <erikagrebur@gmail.com>
2018-08-16 13:07:02 +0200

    The cat follows the camera
    The cat is rotated by the position of the main camera

--- Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
+++ Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
@@ -1,22 +1,3 @@
-//-----------------------------------------------------------------------
-// <copyright file="AugmentedImageVisualizer.cs" company="Google">
-//
-// Copyright 2018 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// </copyright>
-//-----------------------------------------------------------------------
 
 namespace GoogleARCore.Examples.AugmentedImage
 {
@@ -27,20 +8,13 @@ namespace GoogleARCore.Examples.AugmentedImage
     using GoogleARCoreInternal;
     using UnityEngine;
 
-    /// <summary>
-    /// Uses 4 frame corner objects to visualize an AugmentedImage.
-    /// </summary>
+  
     public class AugmentedImageVisualizer : MonoBehaviour
     {
-        /// <summary>
         /// The AugmentedImage to visualize.
-        /// </summary>
         public AugmentedImage Image;
-
-        /// <summary>
-        /// A model for the lower left corner of the frame to place when an image is detected.
-        /// </summary>
-        public GameObject FrameLowerLeft;
+        // Cat model which will be shown
+        public GameObject Cat;
 
         /// <summary>
         /// The Unity Update method.
@@ -49,15 +23,20 @@ namespace GoogleARCore.Examples.AugmentedImage
         {
             if (Image == null || Image.TrackingState != TrackingState.Tracking)
             {
-                FrameLowerLeft.SetActive(false);
+                Cat.SetActive(false);
                 return;
             }
+            GameObject objectToFollow = GameObject.FindWithTag("MainCamera");
+            var lookPos = objectToFollow.transform.position - Cat.transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            Cat.transform.rotation = Quaternion.Slerp(Cat.transform.rotation, rotation, Time.deltaTime * 5F);
 
             float halfWidth = Image.ExtentX / 2;
             float halfHeight = Image.ExtentZ / 2;
-            FrameLowerLeft.transform.localPosition = (halfWidth * Vector3.up) + (halfHeight * Vector3.down);
+            Cat.transform.localPosition = (halfWidth * Vector3.up) + (halfHeight * Vector3.down);
 
-            FrameLowerLeft.SetActive(true);
+            Cat.SetActive(true);
         }
     }
 }
