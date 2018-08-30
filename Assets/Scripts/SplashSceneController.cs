5135641d40f625bd70417a7fb3f6caf624890608
moakprojects <molak8@gmail.com>
2018-08-30 10:59:29 +0200

    splash screen controller added

---
+++ Assets/Scripts/SplashSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(OpenScene());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator OpenScene()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("Slider_first");
+    }
+}
